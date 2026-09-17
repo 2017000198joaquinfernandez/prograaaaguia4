4ea384bcb35e1c1318a62b9478ae6142aec39cfd
2017000198joaquinfernandez <joaquin6cdb@gmail.com>
2026-09-17 12:14:47 -0600

    Add files via upload

---
+++ Guia4-Fernandez2/Form1.cs
@@ -0,0 +1,31 @@
+namespace Guia4_Fernandez2
+{
+    public partial class Form1 : Form
+    {
+        private const int v = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Nombre =
+                int.Parse(textBox1.Text);
+            int Cargo =
+                int.Parse(textBox2.Text);
+
+            string sala = "";
+            switch (sala)
+            {
+                case 1:
+                    sala = "Q5,000";
+                    break;
+
+
+
+            }
+        }
+    }
+}
